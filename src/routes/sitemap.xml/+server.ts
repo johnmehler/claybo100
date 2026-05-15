9928743fa9ab0aae35fb3e7577b93f68995a2b6c
John Mehler <johnmehler@gmail.com>
2026-05-15 19:02:04 -0400

    robots and sitemap

---
+++ src/routes/sitemap.xml/+server.ts
@@ -0,0 +1,39 @@
+import { games } from '$lib/games';
+
+export const prerender = true;
+
+const SITE = 'https://onlinemath.games';
+
+export function GET() {
+	const today = new Date().toISOString().split('T')[0];
+
+	const urls = [
+		{ loc: `${SITE}/`, priority: '1.0', changefreq: 'weekly' },
+		...games.map((g) => ({
+			loc: `${SITE}/games/${g.id}`,
+			priority: '0.8',
+			changefreq: 'monthly'
+		}))
+	];
+
+	const body = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${urls
+	.map(
+		(u) => `	<url>
+		<loc>${u.loc}</loc>
+		<lastmod>${today}</lastmod>
+		<changefreq>${u.changefreq}</changefreq>
+		<priority>${u.priority}</priority>
+	</url>`
+	)
+	.join('\n')}
+</urlset>`;
+
+	return new Response(body, {
+		headers: {
+			'Content-Type': 'application/xml',
+			'Cache-Control': 'max-age=0, s-maxage=3600'
+		}
+	});
+}
